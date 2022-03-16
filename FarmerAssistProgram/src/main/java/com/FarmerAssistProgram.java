077366a7749be124d5192215264c55ee1f915428
wanma <wanma@Aditya_Wanmali>
2022-03-16 08:46:45 +0530

    Second Commit

---
+++ FarmerAssistProgram/src/main/java/com/FarmerAssistProgram.java
@@ -0,0 +1,28 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@SpringBootApplication
+public class FarmerAssistProgram {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmerAssistProgram.class, args);
+		
+		System.out.println("WELCOME FARMING ASSISTANT SYSTEM");
+	}
+	@Bean
+	public Docket productApi() {
+	return new Docket(DocumentationType.SWAGGER_2).select()
+	.apis(RequestHandlerSelectors.basePackage("com")).build();
+	}
+	
+	//http://localhost:8080/swagger-ui/index.html
+}
